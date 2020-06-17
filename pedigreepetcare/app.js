e14a498bf7b75818514c908320726d3bc21ee054
tastethedream <tastethedream@gmail.com>
2020-06-17 19:35:05 +0100

    files set up

---
+++ pedigreepetcare/app.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.send("Hello");
+});
+
+app.listen(8080, function(){
+  console.log("Server started on port 8080.");
+});
