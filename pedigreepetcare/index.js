270a896376264f18852238c271b5f73a094bf5dc
tastethedream <tastethedream@gmail.com>
2020-06-18 11:01:18 +0100

    template set up complete

--- pedigreepetcare/index.js
+++ pedigreepetcare/index.js
@@ -2,16 +2,21 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require ('path');
 
 const app = express();
+
+const pathPublicDirectory = path.join(__dirname, './public');
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(pathPublicDirectory));
+
 
 app.get("/", function(req, res){
-  res.send("Hello");
+  // 
+  res.render ('index');
 });
 
 app.listen(8080, function(){
