09408438533c08ef6993ba13e269c3785de619b2
tastethedream <tastethedream@gmail.com>
2020-06-25 12:27:54 +0100

    completed template

--- templates/basic js css html site/functions.js
+++ templates/basic js css html site/functions.js
@@ -1,1 +1,20 @@
+// function for the menu
+
+function handleNav(){
+    const nav = document.getElementById('main-nav');
+    // function for the hamburger
+    const hamburger = document.getElementById('hamburger');
+
+        if(!nav.classList.contains('main-nav-active')){
+            nav.classList.add('main-nav-active');
+            hamburger.classList.add('hamburger-active');
+        } else {
+            nav.classList.remove('main-nav-active');
+            hamburger.classList.remove('hamburger-active');
+        }
+
+}
+
+
+
 
